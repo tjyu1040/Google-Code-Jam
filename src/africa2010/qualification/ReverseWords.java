0bcdb2559d193408c2c785253adfa47e3559d036
tjyu1040 <tjyu1040@gmail.com>
2014-07-04 17:17:29 -0400

    Fixed bug with newline reading.

--- src/africa2010/qualification/ReverseWords.java
+++ src/africa2010/qualification/ReverseWords.java
@@ -34,6 +34,7 @@ public class ReverseWords {
         PrintWriter outputFile = new PrintWriter(OUTPUT_FILES_PATH + filename.substring(0, filename.length() - 2) + "out");
 
         int testCases = inputFile.nextInt();
+        inputFile.nextLine();   // Get rid of newline
         for (int testCount = 1; testCount <= testCases; testCount++) {
             System.out.print("Case #" + testCount + ": ");
             outputFile.print("Case #" + testCount + ": ");
